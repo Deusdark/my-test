c4ce6a4e7cd38a973194e16e5d5c6fe174574114
Yannick <yannick.gardiol@gmail.com>
2021-12-01 11:28:01 +0100

    Add products

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,3 +1,19 @@
 <template>
   <div>Hello bienvenue sur le meetup Nuxt3 Paris</div>
+
+  <div v-if="error">Une error est survenue</div>
+  <div v-else-if="pending">En cours de chargement</div>
+  <div v-else>
+    <div v-for="product in data">
+      {{ product.title }}
+    </div>
+  </div>
 </template>
+
+<script setup>
+const config = useRuntimeConfig();
+
+const { data, error } = await useAsyncData("products", () =>
+  fetch(`${config.STORE_URL}/products`).then((response) => response.json())
+);
+</script>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,8 @@
-import { defineNuxtConfig } from 'nuxt3'
+import { defineNuxtConfig } from "nuxt3";
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
-})
+  publicRuntimeConfig: {
+    STORE_URL: process.env.STORE_URL || "http://localhost:1337",
+  },
+});
